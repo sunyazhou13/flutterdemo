52e48e682fcdf71adfbdefefc77498fcddf4d839
sunyazhou <sunyazhou13@163.com>
2023-03-05 18:00:17 +0800

    [flutter学习]1. P19 05-(掌握)阶段综合案例的练习 28:46

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,67 @@
 import 'package:flutter/material.dart';
 
-///创建
-void main () {
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget  {
+class MyApp extends StatelessWidget
+{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "hello flutter",
       home: Scaffold(
         appBar: AppBar(
           title: Text("sunyazhou.com"),
         ),
-        body: ContentWidget(),
+        body: HomeContent(),
       ),
     );
   }
 }
 
-class ContentWidget extends StatelessWidget
+class HomeContent extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Hello Flutter!",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontSize: 30,
-            color: Colors.greenAccent
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: ListView(
+        children: [
+          ProductItem("sunyazhou.com A","迈腾大队长 A","https://www.sunyazhou.com/assets/images/20221207RadialGradientlayer/RadialCenter.webp"),
+          ProductItem("sunyazhou.com B","迈腾大队长 B","https://www.sunyazhou.com/assets/images/20230205WebpEnhancement/webp3.webp"),
+          ProductItem("sunyazhou.com C","迈腾大队长 C","https://www.sunyazhou.com/assets/images/20220701ReverseList/algorithm.webp"),
+          ProductItem("sunyazhou.com D","迈腾大队长 D","https://www.sunyazhou.com/assets/images/avatar.jpg"),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget
+{
+  final String title;
+  final String subTitle;
+  final String imageURL;
+
+  ProductItem(this.title, this.subTitle, this.imageURL);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color: Colors.cyanAccent,
         ),
+      ),
+      child: Column(
+        children: [
+          Text(this.title, style: TextStyle(fontSize: 24),),
+          Text(this.subTitle, style: TextStyle(fontSize: 18),),
+          SizedBox(height: 10,),
+          Image.network(this.imageURL),
+        ],
       ),
     );
   }
